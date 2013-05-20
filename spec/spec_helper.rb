7c7da2784a786bbc76ecbf9ec7fc67b47c4c864e
Charles F. Munat <charles.munat@gmail.com>
2013-05-20 12:04:55 +0100

    Added basic rspec test framework and model and controller examples.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,32 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'email_spec'
+require 'rspec/autorun'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+RSpec.configure do |config|
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
+  
+  config.before(:suite) do
+      DatabaseCleaner.strategy = :truncation
+    end
+    
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.use_transactional_fixtures = true
+
+  config.infer_base_class_for_anonymous_controllers = false
+
+  config.order = "random"
+end
